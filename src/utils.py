6e7b09bd49f7351399cd1433638c7ab5e55b8e4a
andrea crotti <kerny404@gmail.com>
2009-04-07 00:21:34 +0200

    Moving in MenuMaker class

--- src/utils.py
+++ src/utils.py
@@ -6,3 +6,41 @@ def clear():
         os.system('cls')
     else:
         os.system('clear')
+        
+
+
+class MenuMaker:
+    """Generates a nice menu"""
+    def __init__(self, choices, key = "val"):
+        self.choices = choices
+        self.key = key
+        self.default = self.choices[0]
+        self.menu = dict(enumerate(self.choices))
+        
+    def __str__(self):
+        return '\n'.join([str(i) + ")\t" + str(self.menu[i]) for i in range(len(self.choices))])
+    
+    def __getitem__(self, idx):
+        if self.key == "val":
+            return self.menu[idx]
+        elif self.key == 'idx':
+            return idx
+        
+# TODO Use dialog if available
+def menu_set(menu):
+    while True:
+        print str(menu)
+        val = raw_input("make a choice (default %s):\n\n" % str(menu.default))
+        if val == '':
+            if menu.key == "val":
+                return self.default
+            else:
+                return 0
+        else:
+            try:
+                return menu[int(val)]
+            except KeyError:
+                continue
+            except ValueError:
+                print "you must give integer input"
+                continue
