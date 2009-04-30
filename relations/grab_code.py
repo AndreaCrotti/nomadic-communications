8c1ff15d30947388f9295ff38f52e7949df90927
Andrea Crotti <kerny404@gmail.com>
2009-04-30 20:15:53 +0000

    generalize graph template

---
+++ relations/grab_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Useful script to insert source code
+# in the latex relation, using mako + pygment + inspect
+
+import sys
+import inspect
+import logging
+
+sys.path.append('../src')
+
+def get_code(objcode):
+    try:
+	mod = __import__(objcode)
+    except ImportError:
+	logging.error("could not find %s" % objcode)
+	sys.exit(1)    
+    else:
+        return inspect.getsource(mod)
+
+if __name__ == '__main__':
+    get_code(sys.argv[1])
---
+++ relations/make_result.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# TODO encoding line
+
+import sys, os
+
